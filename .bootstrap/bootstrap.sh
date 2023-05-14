137f6e311ffcc94e3c355866cac1768bd09f5182
imawizard <imawizard@outlook.de>
2023-05-15 01:01:26 +0200

    Add terminfo/tmux-256color
    Taken from
     - https://gpanders.com/blog/the-definitive-guide-to-using-tmux-256color-on-macos/
     - https://github.com/tmux/tmux/issues/377#issuecomment-212541169
     - https://gist.github.com/sos4nt/3187620

--- .bootstrap/bootstrap.sh
+++ .bootstrap/bootstrap.sh
@@ -567,6 +567,14 @@ if [[ $(command -v tmux) ]]; then
     test -x ~/.tmux/plugins/tpm/bin/install_plugins && "$_"
 fi
 
+# Possibly add missing tmux-256color.
+test ! -f ~/.terminfo/*/tmux-256color && \
+    tee <<HERE $(basename "$_").terminfo >/dev/null && tic "$_" && rm -f "$_"
+tmux-256color|screen with 256 colors and italic,
+  sitm=\E[3m, ritm=\E[23m,
+  use=screen-256color,
+HERE
+
 echo "Installing Quicklook & Services..."
 find "$ICLOUD_DRIVE/.config/Apps/Quicklook" -name '*.qlgenerator' -maxdepth 1 -exec cp -a -n {} "$HOME/Library/Quicklook/" \;
 find "$ICLOUD_DRIVE/.config/Apps/Services" -name '*.workflow' -maxdepth 1 -exec cp -a -n {} "$HOME/Library/Services/" \;
